6f6e4458c5a0b0ba405429b67b31373f2f0a40ee
MukeshkumarA <mukesh.arumugam@claysys.co>
2024-03-05 00:16:06 +0530

    feat: lazy loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from './components/Header';
 import Body from './components/Body';
@@ -6,13 +6,15 @@ import Footer from './components/Footer';
 import About from "./components/About";
 import Error from "./components/Error";
 import Contact from "./components/Contact";
-import RestaurantMenu from "./components/RestaurantMenu";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Login from "./components/Login";
 import { ThemeProvider, UserProvider } from "./utils/ContextProvider";
 import { Provider } from "react-redux";
 import store from "./utils/Store";
-import Cart from "./components/Cart";
+
+// chunking | code splitting | dynamic bundling | lazy loading | on demand loading | dynamic import
+const Cart = lazy(() => import("./components/Cart"));
+const RestaurantMenu = lazy(() => import("./components/RestaurantMenu"));
 
 const AppLayout = () => {
   return (
@@ -37,9 +39,9 @@ const appRouter = createBrowserRouter([
       { path: "/", element: <Body />, errorElement: <Error /> },
       { path: "/about", element: <About />, errorElement: <Error /> },
       { path: "/contact", element: <Contact />, errorElement: <Error /> },
-      { path: "/restaurant/:restaurantId", element: <RestaurantMenu />, errorElement: <Error /> },
+      { path: "/restaurant/:restaurantId", element: <Suspense><RestaurantMenu /></Suspense>, errorElement: <Error /> },
       { path: "/login", element: <Login />, errorElement: <Error /> },
-      { path: "/cart", element: <Cart />, errorElement: <Error />}
+      { path: "/cart", element: <Suspense fallback={`Loading............`}><Cart /></Suspense>, errorElement: <Error />}
     ],
   }
 ]);
